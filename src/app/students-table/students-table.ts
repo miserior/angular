5ec2689da7d441cb7fa59e677438c380ab28170c
miserior <51886505+miserior@users.noreply.github.com>
2025-07-22 14:12:44 -0500

    feature: cambios ui

--- src/app/students-table/students-table.ts
+++ src/app/students-table/students-table.ts
@@ -2,14 +2,16 @@ import { Component, Input } from '@angular/core';
 import { Student } from '../../shared/entities';
 import { MatTableModule } from '@angular/material/table';
 import { FullnamePipe } from '../../shared/pipes/fullname-pipe';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-students-table',
-  imports: [MatTableModule, FullnamePipe],
+  imports: [MatTableModule, FullnamePipe,MatFormFieldModule, MatInputModule],
   templateUrl: './students-table.html',
   styleUrl: './students-table.css'
 })
-export class StudentsTable {
+export class StudentsTable  {
 @Input() students: Student[] = []; 
 displayedColumns: string[] = ['fullname', 'age', 'dni', 'average'];
 }
