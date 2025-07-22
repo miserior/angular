c9bd66a09043dcb10ea786010bc0c5c50218a9f0
miserior <51886505+miserior@users.noreply.github.com>
2025-07-21 20:29:04 -0500

    feature: add get http

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Toolbar } from './toolbar/toolbar';
 import { Navbar } from './navbar/navbar';
+import { HttpClient } from '@angular/common/http';
+import { Student } from '../shared/entities';
+import { CommonModule } from '@angular/common';
+import { StudentsTable } from "./students-table/students-table";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Toolbar, Navbar],
+  imports: [RouterOutlet, Toolbar, Navbar, CommonModule, StudentsTable],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('entregable');
+  students: Student[] = [];
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<Student[]>('/mocks/students.json').subscribe((data) => {
+      this.students = data;
+    });
+  }
 }
